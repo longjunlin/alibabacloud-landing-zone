ace799738d4160e98886fd6c9b29b82e2c92090d
longjun.lin <longjun.lin@alibaba-inc.com>
2022-04-06 19:02:55 +0800

    add example

--- solution/automation/account-factory/modules/idp/examples/complete/main.tf
+++ solution/automation/account-factory/modules/idp/examples/complete/main.tf
@@ -1,16 +1,16 @@
 terraform {
-  required_providers {
-    alicloud = {
-      source = "aliyun/alicloud"
-      version = "1.127.0"
-    }
-
-    local = {
-      source = "hashicorp/local"
-      version = "2.1.0"
-    }
-  }
-  required_version = ">=0.12"
+#  required_providers {
+#    alicloud = {
+#      source = "aliyun/alicloud"
+#      version = "1.127.0"
+#    }
+#
+#    local = {
+#      source = "hashicorp/local"
+#      version = "2.1.0"
+#    }
+#  }
+#  required_version = ">=0.12"
 
   backend "oss" {
     bucket              = "bucket-with-terraform-state1"
