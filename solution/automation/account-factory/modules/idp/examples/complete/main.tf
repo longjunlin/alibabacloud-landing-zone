a7bc96d9a2db071c7c8d6483476edfb883c4dd22
longjun.lin <longjun.lin@alibaba-inc.com>
2022-04-06 17:56:40 +0800

    add example

---
+++ solution/automation/account-factory/modules/idp/examples/complete/main.tf
@@ -0,0 +1,18 @@
+provider "alicloud" {
+  alias = "rd_role"
+  assume_role {
+    role_arn           = format("acs:ram::%s:role/ResourceDirectoryAccountAccessRole", var.account_id)
+    session_name       = "AccountLandingZoneSetup"
+    session_expiration = 999
+  }
+}
+
+module "idp" {
+  source = "../../"
+
+  providers = {
+    alicloud = alicloud.rd_role
+  }
+  sso_provider_name = var.sso_provider_name
+  encodedsaml_metadata_document = var.encodedsaml_metadata_document
+}
