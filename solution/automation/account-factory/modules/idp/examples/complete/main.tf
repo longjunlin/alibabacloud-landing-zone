b85cd27291020ce6650a41319b1d592a503cc45b
longjun.lin <longjun.lin@alibaba-inc.com>
2022-04-07 11:54:58 +0800

    modify step name

--- solution/automation/account-factory/modules/idp/examples/complete/main.tf
+++ solution/automation/account-factory/modules/idp/examples/complete/main.tf
@@ -5,13 +5,6 @@ terraform {
     }
   }
   backend "oss" {
-#    bucket              = "bucket-with-terraform-state1"
-#    prefix              = "path/mystate"
-#    key                 = "{state_key}"
-#    region              = "cn-hangzhou"
-#    tablestore_endpoint = "https://terraform-hz-1.cn-hangzhou.ots.aliyuncs.com"
-#    tablestore_table    = "statelock"
-#    endpoint            = "oss-cn-hangzhou.aliyuncs.com"
   }
 }
 
